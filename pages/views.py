61ca1ee36c455fe2f90b6f37313ab7e6959afbe2
Tung <nh.tung@outlook.com>
2021-07-24 21:59:30 +0700

    Completed Teams Model

--- pages/views.py
+++ pages/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
-
+from .models import Team
 # Create your views here.
 
 def home(request):
     return render(request, 'pages/home.html')
 
 def about(request):
-    return render(request, 'pages/about.html')
+    teams = Team.objects.all()
+    data = {
+        'teams': teams,
+    }
+    return render(request, 'pages/about.html', data)
+## 'teams' is the name of variable teams, be used in other pages, commented by Tung
 
 def services(request):
     return render(request, 'pages/services.html')

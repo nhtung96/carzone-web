509b0cad344a26688d368ab8ec7fac919abd7cdd
Tung <nh.tung@outlook.com>
2021-08-08 12:41:22 +0700

    Added cars app and fetched data into homepage

---
+++ cars/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from .models import Car
+from django.utils.html import format_html
+# Register your models here.
+
+class CarAdmin(admin.ModelAdmin):
+    def thumbnail(self, object):
+        return format_html('<img src="{}" width="40"/>'.format(object.car_photo.url))
+    thumbnail.short_description = 'Car Image'
+    list_display = ('id', 'thumbnail', 'car_title', 'color', 'model', 'year', 'body_style', 'fuel_type', 'is_featured')
+    list_display_links = ('id', 'car_title','thumbnail')
+    list_editable = ('is_featured',)
+    search_fields = ('id', 'car_title', 'fuel_type', 'year', 'city', 'model', 'body_style')
+    list_filter = ('city', 'model', 'body_style','fuel_type')
+admin.site.register(Car, CarAdmin)
